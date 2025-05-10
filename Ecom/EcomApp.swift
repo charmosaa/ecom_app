36b6d34e407e949a1c48ecd116fb7d216f8969d5
charmosaa <272682@student.pwr.edu.pl>
2025-05-10 09:47:55 +0200

    Initial commit

---
+++ Ecom/EcomApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomApp.swift
+//  Ecom
+//
+//  Created by Martyna Lopianiak on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct EcomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcomTests/EcomTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcomTests.swift
+//  EcomTests
+//
+//  Created by Martyna Lopianiak on 10/05/2025.
+//
+
+import Testing
+@testable import Ecom
+
+struct EcomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
